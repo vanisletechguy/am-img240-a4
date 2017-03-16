1e82d79a689e04686ddef34ec85177e23bd604be
majajobee <mjaubie@gmail.com>
2017-03-15 22:34:58 -0700

    added generator with permissions

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -1,17 +1,56 @@
 class GeneratorController < ApplicationController
-  def generate_data
+  #before_action :set_phone, only: [:delete_data, :generate_data]
+  before_action :authenticate_user!
+  #before_action :set_user, :set_generator
 
+  def generate_data
+    authorize :generator, :generate_data?
     Faker::Config.locale = 'en-US'
     5.times do
+      new_user = User.create!(admin: false,
+                    email: Faker::Internet.free_email,
+                    password: 'abc1234')
       Phone.create!(name: Faker::Name.name,
                     number: Faker::PhoneNumber.phone_number,
-                    city: Faker::Address.city, user_id: 1)
-      User.create!(admin: false,
-                    email: Faker::Internet.email,
-                    password: 'abc1234')
+                    city: Faker::Address.city,
+                    user_id: new_user.id)
     end
-
-
     redirect_to phones_path
   end
+  def delete_data
+    #authorize @generator
+    Phone.all.each do |phone|
+      phone.destroy
+    end
+    User.all.each do |user|
+      if user.admin != true
+        user.destroy
+      end
+    end
+    redirect_to phones_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(current_user)
+    end
+
+    # def set_phone
+    #   @phone = Phone.find(params[:id])
+    # end
+    #
+    # # Never trust parameters from the scary internet, only allow the white list through.
+    # def phone_params
+    #   params.require(:phone).permit(:name, :number, :city, :user_id)
+    # end
+
+    def set_generator
+      @generator = @user
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'generator/generate_data'
+  get 'generator/delete_data'
   root 'welcome#index'
   resources :phones
   devise_for :users
